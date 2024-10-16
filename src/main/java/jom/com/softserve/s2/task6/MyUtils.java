e4dda59b8100d4e4681f5cda144068dbad77a6d5
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2024-10-16 11:10:03 +0200

    Good

--- src/main/java/jom/com/softserve/s2/task6/MyUtils.java
+++ src/main/java/jom/com/softserve/s2/task6/MyUtils.java
@@ -1,10 +1,48 @@
 package jom.com.softserve.s2.task6;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyUtils {
 
-    //Write your code here
+    public  List<Shape> maxAreas(List<Shape> shapes) {
+        if (shapes.isEmpty()) return new ArrayList<>();
+        for (Shape shape : shapes) {
+            if (shape == null) return new ArrayList<>();
+        }
 
+
+        List<Shape> result = new ArrayList<>();
+
+        double maxCircleArea = 0.0;
+        double maxRectangleArea = 0.0;
+
+        for (Shape shape : shapes) {
+            if (shape instanceof Circle) {
+                double area = shape.getArea();
+                if (area > maxCircleArea) {
+                    maxCircleArea = area;
+                }
+            } else if (shape instanceof Rectangle) {
+                double area = shape.getArea();
+                if (area > maxRectangleArea) {
+                    maxRectangleArea = area;
+                }
+            }
+
+        }
+
+        for (Shape shape : shapes) {
+            if (shape instanceof Circle) {
+                if (shape.getArea() == maxCircleArea) {
+                    result.add(shape);
+                }
+            } else if (shape instanceof Rectangle) {
+                if (shape.getArea() == maxRectangleArea) {
+                    result.add(shape);
+                }
+            }
+        }
+        return result;
+    }
 }
--- src/main/java/jom/com/softserve/s2/task6/Shape.java
+++ src/main/java/jom/com/softserve/s2/task6/Shape.java
@@ -3,6 +3,9 @@ package jom.com.softserve.s2.task6;
 public abstract class Shape {
     private String name;
 
+    public Shape() {
+    }
+
     public Shape(String name) {
         this.name = name;
     }
@@ -12,21 +15,104 @@ public abstract class Shape {
     }
 
     public double getCircleArea() {
-        //Code
+
         return 0.0;
     }
 
-    public double getRectangleArea() {
-        //Code
-        return 0.0;
+    public abstract double getArea();
+
+}
+
+
+class Circle extends Shape {
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
     }
 
+    public Circle(String name, double radius) {
+        super(name);
+        this.radius = radius;
+    }
+
+    @Override
     public double getArea() {
-        if (getName().equals("Circle")) {
-            return getCircleArea();
-        } else {
-            return getRectangleArea();
-        }
-        //Code
+        return Math.PI * radius * radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Circle{");
+        sb.append("radius=").append(radius);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Circle circle = (Circle) o;
+        return radius == circle.radius;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (radius * 31);
+    }
+}
+
+class Rectangle extends Shape {
+    private final double height;
+    private final double width;
+
+
+    public Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public Rectangle(String name, double height, double width) {
+        super(name);
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
+    public double getArea() {
+        return height * width;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Rectangle [height=%.2f, width=%.2f]", height, width);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return (int) ((height + width) * 31);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return (height == rectangle.height && width == rectangle.width) ||
+               (height == rectangle.width && width == rectangle.height);
     }
 }
